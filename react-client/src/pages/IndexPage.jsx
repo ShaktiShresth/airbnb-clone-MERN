f87e6fc30ff2b8e1ce9f817662a5730cbda3814c
Shakti Shrestha <75934459+ShaktiShresth@users.noreply.github.com>
2023-12-30 10:16:13 +0545

    pages modification

--- react-client/src/pages/IndexPage.jsx
+++ react-client/src/pages/IndexPage.jsx
@@ -2,8 +2,8 @@ import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import Footer from "..//components/Footer";
-import LoadingBar from 'react-top-loading-bar';
-import Spinner from '../components/Spinner';
+import LoadingBar from "react-top-loading-bar";
+import Spinner from "../components/Spinner";
 import { UserContext } from "../UserContext";
 
 export default function IndexPage() {
@@ -14,21 +14,21 @@ export default function IndexPage() {
   const [progress, setProgress] = useState(8);
   const [loader, setLoader] = useState(true);
 
-  const {user} = useContext(UserContext);
+  const { user } = useContext(UserContext);
   let navigate = useNavigate();
 
   useEffect(() => {
-    if(user){
-    setTimeout(() => {
-      axios.get('/places').then(response => {
-        setPlaces(response.data);
-      });
-      setProgress(100);
-      setLoader(false);
-    }, 500)
-  }else{
-    navigate("/login")
-  }
+    if (user) {
+      setTimeout(() => {
+        axios.get("/places").then((response) => {
+          setPlaces(response.data);
+        });
+        setProgress(100);
+        setLoader(false);
+      }, 500);
+    } else {
+      navigate("/login");
+    }
   }, []);
 
   //pagination
@@ -40,112 +40,154 @@ export default function IndexPage() {
 
   const visiblePlaces = places.slice(indexOfFirstPlace, indexOfLastPlace);
 
-  const inactiveClass = "px-4 py-2 mx-1 rounded-xl cursor-pointer hover:bg-primaryHoverColor transition delay-100 ease-in";
+  const inactiveClass =
+    "px-4 py-2 mx-1 rounded-xl cursor-pointer hover:bg-primaryHoverColor transition delay-100 ease-in";
   const activeClass = `${inactiveClass} + bg-primaryColor + font-semibold + text-white`;
 
-  function prevPageHandler(){
-    if(currentPage !== 1) setCurrentPage(currentPage - 1);
+  function prevPageHandler() {
+    if (currentPage !== 1) setCurrentPage(currentPage - 1);
   }
-  function nextPageHandler(){
-    if(currentPage !== numOfTotalPages) setCurrentPage(currentPage + 1);
+  function nextPageHandler() {
+    if (currentPage !== numOfTotalPages) setCurrentPage(currentPage + 1);
   }
 
-    return (
-      <div className="relative">
-        <LoadingBar
-        color='#f11946'
+  return (
+    <div className="relative">
+      <LoadingBar
+        color="#f11946"
         progress={progress}
         onLoaderFinished={() => setProgress(0)}
       />
-     
-        <div className="p-4 w-96 m-auto relative">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 absolute left-6 top-8 text-primaryColor">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-          </svg>
-          <input onChange={(ev) => {setSearchTerm(ev.target.value)}} 
-                  id="search-places" 
-                  className="search-places" 
-                  type="text" 
-                  placeholder="Search..."/>
-        </div>
-        <div className="flex justify-center">
-          {loader && <Spinner/> }
-        </div>
-        <div className="px-4 py-8 lg:px-20 grid gap-y-8 gap-x-6 grid-cols-2 max-[430px]:grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
-          {
-             searchTerm? places.filter((place) => {
-              if(searchTerm == ""){
-                  return  place;
-              }else if(place.address.toLowerCase().includes(searchTerm.toLowerCase()) || place.title.toLowerCase().includes(searchTerm.toLowerCase())){
+
+      <div className="p-4 w-96 m-auto relative">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6 absolute left-6 top-8 text-primaryColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+          />
+        </svg>
+        <input
+          onChange={(ev) => {
+            setSearchTerm(ev.target.value);
+          }}
+          id="search-places"
+          className="search-places"
+          type="text"
+          placeholder="Search..."
+        />
+      </div>
+      <div className="flex justify-center">{loader && <Spinner />}</div>
+      <div className="min-h-screen px-4 py-8 lg:px-20 grid gap-y-8 gap-x-6 grid-cols-2 max-[430px]:grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
+        {searchTerm
+          ? places
+              .filter((place) => {
+                if (searchTerm == "") {
                   return place;
-              }
-            }).map((place, index) => {
+                } else if (
+                  place.address
+                    .toLowerCase()
+                    .includes(searchTerm.toLowerCase()) ||
+                  place.title.toLowerCase().includes(searchTerm.toLowerCase())
+                ) {
+                  return place;
+                }
+              })
+              .map((place, index) => {
+                return (
+                  <Link to={"/place/" + place._id} key={index}>
+                    <div className="bg-gray-500 rounded-2xl flex mb-2">
+                      {place.photos?.[0] && (
+                        <img
+                          className="rounded-2xl object-cover aspect-square hover:scale-105"
+                          src={
+                            "http://localhost:4000/uploads/" + place.photos?.[0]
+                          }
+                          alt=""
+                        />
+                      )}
+                    </div>
+                    <h2 className="font-bold truncate">{place.address}</h2>
+                    <h3 className="text-gray-500 text-sm truncate">
+                      {place.title}
+                    </h3>
+                    <div className="text-gray-500 mt-1">
+                      <span className="text-black font-bold">
+                        ${place.price}
+                      </span>{" "}
+                      night
+                    </div>
+                  </Link>
+                );
+              })
+          : visiblePlaces.map((place, index) => {
               return (
-                  <Link to={'/place/'+place._id} key={index}>
+                <Link to={"/place/" + place._id} key={index}>
                   <div className="bg-gray-500 rounded-2xl flex mb-2">
                     {place.photos?.[0] && (
-                      <img className="rounded-2xl object-cover aspect-square hover:scale-105" src={'http://localhost:4000/uploads/' + place.photos?.[0]} alt=""/>
+                      <img
+                        className="rounded-2xl object-cover aspect-square hover:scale-105 transition duration-200 ease-in hover:delay-150"
+                        src={
+                          "http://localhost:4000/uploads/" + place.photos?.[0]
+                        }
+                        alt=""
+                      />
                     )}
                   </div>
                   <h2 className="font-bold truncate">{place.address}</h2>
-                  <h3 className="text-gray-500 text-sm truncate">{place.title}</h3>
+                  <h3 className="text-gray-500 text-sm truncate">
+                    {place.title}
+                  </h3>
                   <div className="text-gray-500 mt-1">
-                    <span className="text-black font-bold">${place.price}</span> night
+                    <span className="text-black font-bold">${place.price}</span>{" "}
+                    night
                   </div>
                 </Link>
-              )
-            }) 
-            : 
-            visiblePlaces.map((place, index) => {
-              return (
-                <Link to={'/place/'+place._id} key={index}>
-                    <div className="bg-gray-500 rounded-2xl flex mb-2">
-                    {place.photos?.[0] && (
-                      <img className="rounded-2xl object-cover aspect-square hover:scale-105 transition duration-200 ease-in hover:delay-150" src={'http://localhost:4000/uploads/' + place.photos?.[0]} alt=""/>
-                    )}
-                  </div>
-                  <h2 className="font-bold truncate">{place.address}</h2>
-                  <h3 className="text-gray-500 text-sm truncate">{place.title}</h3>
-                  <div className="text-gray-500 mt-1">
-                    <span className="text-black font-bold">${place.price}</span> night
-                  </div>
-                </Link>
-              )
-            })
-          }
-        </div>
-        {/* pagination area below*/}
-        <div className="text-sm md:text-base">
-          {
-            places.length > 0 &&  numOfTotalPages !== 1 && (
-              <div className="flex justify-center items-center my-4">
-                <span onClick={prevPageHandler} 
-                      disabled = {currentPage === 1}
-                      className="font-semibold px-4 py-2 bg-gray-200 rounded-md cursor-pointer hover:bg-primaryHoverColor hover:text-white transition delay-100 ease-in">
-                        Prev
-                </span>
-                <p className="text-center">
-                  {
-                    pages.map((page) => (
-                      <span key={page} 
-                            onClick={() => setCurrentPage(page)} 
-                            className={`${currentPage === page ? activeClass : inactiveClass }`}>
-                        {page}
-                      </span>
-                    ))
-                  }
-                </p>
-                <span onClick={nextPageHandler} 
-                      disabled={currentPage === numOfTotalPages}
-                      className="font-semibold px-4 py-2 bg-gray-200 rounded-md cursor-pointer hover:bg-primaryHoverColor hover:text-white transition delay-100 ease-in">
-                        Next
-                </span>
-            </div>
-            )
-          }
-        </div>
-        <Footer />
+              );
+            })}
       </div>
-    )
-  }
-  
+      {/* pagination area below*/}
+      <div className="text-sm md:text-base">
+        {places.length > 0 && numOfTotalPages !== 1 && (
+          <div className="flex justify-center items-center my-4">
+            <span
+              onClick={prevPageHandler}
+              disabled={currentPage === 1}
+              className="font-semibold px-4 py-2 bg-gray-200 rounded-md cursor-pointer hover:bg-primaryHoverColor hover:text-white transition delay-100 ease-in"
+            >
+              Prev
+            </span>
+            <p className="text-center">
+              {pages.map((page) => (
+                <span
+                  key={page}
+                  onClick={() => setCurrentPage(page)}
+                  className={`${
+                    currentPage === page ? activeClass : inactiveClass
+                  }`}
+                >
+                  {page}
+                </span>
+              ))}
+            </p>
+            <span
+              onClick={nextPageHandler}
+              disabled={currentPage === numOfTotalPages}
+              className="font-semibold px-4 py-2 bg-gray-200 rounded-md cursor-pointer hover:bg-primaryHoverColor hover:text-white transition delay-100 ease-in"
+            >
+              Next
+            </span>
+          </div>
+        )}
+      </div>
+      <Footer />
+    </div>
+  );
+}
--- react-client/src/pages/PlacesFormPage.jsx
+++ react-client/src/pages/PlacesFormPage.jsx
@@ -7,135 +7,178 @@ import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
 
 export default function PlacesFormPage() {
-    const {id} = useParams();
-    const [title, setTitle] = useState('');
-    const [address, setAddress] = useState('');
-    const [description, setDescription] = useState('');
-    const [addedPhotos, setAddedPhotos] = useState([]);
-    const [perks, setPerks] = useState([]);
-    const [extraInfo, setExtraInfo] = useState('');
-    const [checkIn, setCheckIn] = useState('');
-    const [checkOut, setCheckOut]=useState('');
-    const [maxGuests, setMaxGuests] = useState(1);
-    const [price, setPrice] = useState(100);
-    const [redirect, setRedirect] = useState(false);
+  const { id } = useParams();
+  const [title, setTitle] = useState("");
+  const [address, setAddress] = useState("");
+  const [description, setDescription] = useState("");
+  const [addedPhotos, setAddedPhotos] = useState([]);
+  const [perks, setPerks] = useState([]);
+  const [extraInfo, setExtraInfo] = useState("");
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  const [maxGuests, setMaxGuests] = useState(1);
+  const [price, setPrice] = useState(100);
+  const [redirect, setRedirect] = useState(false);
 
-    useEffect(() => {
-        if(!id){
-            return;
-        }
-        axios.get('/places/'+id).then(response => {
-            const {data} = response;
-            setTitle(data.title);
-            setAddress(data.address);
-            setAddedPhotos(data.photos);
-            setDescription(data.description);
-            setPerks(data.perks);
-            setExtraInfo(data.extraInfo);
-            setCheckIn(data.checkIn);
-            setCheckOut(data.checkOut);
-            setMaxGuests(data.maxGuests);
-            setPrice(data.price)
-        });
-    }, [id]);
+  useEffect(() => {
+    if (!id) {
+      return;
+    }
+    axios.get("/places/" + id).then((response) => {
+      const { data } = response;
+      setTitle(data.title);
+      setAddress(data.address);
+      setAddedPhotos(data.photos);
+      setDescription(data.description);
+      setPerks(data.perks);
+      setExtraInfo(data.extraInfo);
+      setCheckIn(data.checkIn);
+      setCheckOut(data.checkOut);
+      setMaxGuests(data.maxGuests);
+      setPrice(data.price);
+    });
+  }, [id]);
 
-    const inputHeader = (text) => {
-        return (                    
-            <h2 className="mt-4 text-xl truncate">{text}</h2>
-        )
-    }
-    const inputDescription = (text) => {
-        return (
-            <p className="text-sm text-gray-500 truncate">{text}</p>
-        )
-    }
-    const preInput = (header, description) => {
-        return (
-            <>
-                {inputHeader(header)}
-                {inputDescription(description)}
-            </>
-        )
-    }
-    async function savePlace(ev){
-        ev.preventDefault();
-        if(!title || !address || !description || !checkIn || !checkOut || !maxGuests || !price){
-            toast.warning("Please enter all the required info to add the place to your account.", {
-                position: toast.POSITION.BOTTOM_RIGHT
-              });
-              return;
+  const inputHeader = (text) => {
+    return <h2 className="mt-4 text-xl truncate">{text}</h2>;
+  };
+  const inputDescription = (text) => {
+    return <p className="text-sm text-gray-500 truncate">{text}</p>;
+  };
+  const preInput = (header, description) => {
+    return (
+      <>
+        {inputHeader(header)}
+        {inputDescription(description)}
+      </>
+    );
+  };
+  async function savePlace(ev) {
+    ev.preventDefault();
+    if (
+      !title ||
+      !address ||
+      !description ||
+      !checkIn ||
+      !checkOut ||
+      !maxGuests ||
+      !price
+    ) {
+      toast.warning(
+        "Please enter all the required info to add the place to your account.",
+        {
+          position: toast.POSITION.BOTTOM_RIGHT,
         }
-        const placeDetails = {
-            title, address, addedPhotos, description,
-            perks, extraInfo, checkIn, checkOut, maxGuests, price,
-        }
-        if(id){
-            //update place
-            await axios.put('/places', {id, ...placeDetails});
-            setRedirect(true);
-        }else{
-            //add new place
-            await axios.post('/places', placeDetails);
-            setRedirect(true);
-        } 
+      );
+      return;
     }
+    const placeDetails = {
+      title,
+      address,
+      addedPhotos,
+      description,
+      perks,
+      extraInfo,
+      checkIn,
+      checkOut,
+      maxGuests,
+      price,
+    };
+    if (id) {
+      //update place
+      await axios.put("/places", { id, ...placeDetails });
+      setRedirect(true);
+    } else {
+      //add new place
+      await axios.post("/places", placeDetails);
+      setRedirect(true);
+    }
+  }
 
-    if(redirect){
-        return <Navigate to={'/account/places'} />;
-    }    
+  if (redirect) {
+    return <Navigate to={"/account/places"} />;
+  }
 
   return (
     <div className="px-20 pb-4 pt-4">
-        <AccountNav />
-        <form onSubmit={savePlace}>
-            {preInput('Title', 'Title of your place, make it short and appealing')}
-            <input type="text" value={title || ""} onChange={(ev)=>setTitle(ev.target.value)} placeholder="title"/>
-            {preInput('Address', 'Address to the place')}
-            <input type="text" value={address || ""} onChange={(ev)=>setAddress(ev.target.value)} placeholder="address"/>
-            {preInput('Photos', 'Photos of the place')}
-            <PhotoUploader addedPhotos={addedPhotos} onChange={setAddedPhotos}/>
-            {preInput('Description', 'Relative information of the place')}
-            <textarea value={description || ""} onChange={(ev)=>setDescription(ev.target.value)} rows={3}/>
-            {preInput('Perks', 'Select all the perks of the place')}
-            <Perks selected={perks} onChange={setPerks}/>
-            {preInput('Extra Information', 'Notices, house rules, etc...')}
-            <textarea value={extraInfo || ""} onChange={(ev)=>setExtraInfo(ev.target.value)} rows="3" />
-            {preInput('Check in & check out times', 'Add check in and out times')}
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-2">
-                <div>
-                    <h3 className="mt-2 -mb-1 truncate">Check-in time(24hrs)</h3>
-                    <input type="text" 
-                            value={checkIn || ""} 
-                            onChange={(ev)=>setCheckIn(ev.target.value)} 
-                            placeholder="12"/>
-                </div>
-                <div>
-                    <h3 className="mt-2 -mb-1 truncate">Check-out time(24hrs)</h3>
-                    <input type="text" 
-                            value={checkOut || ""} 
-                            onChange={(ev)=>setCheckOut(ev.target.value)} 
-                            placeholder="24"/>
-                </div>
-                <div>
-                    <h3 className="mt-2 -mb-1 truncate">Maximum guests</h3>
-                    <input type="number" min={1} max={50}
-                            value={maxGuests || ""} 
-                            onChange={(ev)=>setMaxGuests(ev.target.value)}/>
-                </div>
-                <div>
-                    <h3 className="mt-2 -mb-1 truncate">Price per night($)</h3>
-                    <input type="number"
-                            value={price || ""} 
-                            onChange={(ev)=>setPrice(ev.target.value)}/>
-                </div>
-            </div>
-            <div className="flex items-center justify-center">
-            <button className="my-4 block py-2 w-96 text-xl text-white text-center bg-primaryColor rounded-2xl hover:bg-primaryHoverColor">
-                    Save
-            </button>
-            </div>
-        </form>
-        <ToastContainer />
+      <AccountNav />
+      <form onSubmit={savePlace}>
+        {preInput("Title", "Title of your place, make it short and appealing")}
+        <input
+          type="text"
+          value={title || ""}
+          onChange={(ev) => setTitle(ev.target.value)}
+          placeholder="title"
+        />
+        {preInput("Address", "Address to the place")}
+        <input
+          type="text"
+          value={address || ""}
+          onChange={(ev) => setAddress(ev.target.value)}
+          placeholder="address"
+        />
+        {preInput("Photos", "Photos of the place")}
+        <PhotoUploader addedPhotos={addedPhotos} onChange={setAddedPhotos} />
+        {preInput("Description", "Relative information of the place")}
+        <textarea
+          value={description || ""}
+          onChange={(ev) => setDescription(ev.target.value)}
+          rows={3}
+        />
+        {preInput("Perks", "Select all the perks of the place")}
+        <Perks selected={perks} onChange={setPerks} />
+        {preInput("Extra Information", "Notices, house rules, etc...")}
+        <textarea
+          value={extraInfo || ""}
+          onChange={(ev) => setExtraInfo(ev.target.value)}
+          rows="3"
+        />
+        {preInput("Check in & check out times", "Add check in and out times")}
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-2">
+          <div>
+            <h3 className="mt-2 -mb-1 truncate">Check-in time(24hrs)</h3>
+            <input
+              type="text"
+              value={checkIn || ""}
+              onChange={(ev) => setCheckIn(ev.target.value)}
+              placeholder="12"
+            />
+          </div>
+          <div>
+            <h3 className="mt-2 -mb-1 truncate">Check-out time(24hrs)</h3>
+            <input
+              type="text"
+              value={checkOut || ""}
+              onChange={(ev) => setCheckOut(ev.target.value)}
+              placeholder="24"
+            />
+          </div>
+          <div>
+            <h3 className="mt-2 -mb-1 truncate">Maximum guests</h3>
+            <input
+              type="number"
+              min={1}
+              max={50}
+              value={maxGuests || ""}
+              onChange={(ev) => setMaxGuests(ev.target.value)}
+            />
+          </div>
+          <div>
+            <h3 className="mt-2 -mb-1 truncate">Price per night($)</h3>
+            <input
+              type="number"
+              value={price || ""}
+              onChange={(ev) => setPrice(ev.target.value)}
+            />
+          </div>
+        </div>
+        <div className="flex items-center justify-center">
+          <button className="my-4 block py-2 w-96 text-xl text-white text-center bg-primaryColor rounded-2xl hover:bg-primaryHoverColor">
+            Save
+          </button>
+        </div>
+      </form>
+      <ToastContainer />
     </div>
-  )
+  );
 }
